91f3dcdf0ec6e8253b3067397902fab5bf966dfc
Douglas Urner <dlu@canishe.com>
2019-04-26 14:36:16 -0700

    Add Barn Blaster example

---
+++ Barn Blaster/Final Project/Assets/RW/Scripts/Tractor.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * Copyright (c) 2018 Razeware LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
+ * distribute, sublicense, create a derivative work, and/or sell copies of the 
+ * Software in any work that is designed, intended, or marketed for pedagogical or 
+ * instructional purposes related to programming, coding, application development, 
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works, 
+ * or sale is expressly withheld.
+ *    
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class Tractor : MonoBehaviour
+{
+    public int speed;
+    private bool isMovingLeft = false;
+    public AudioClip deathSound;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (isMovingLeft)
+        {
+            transform.position = new Vector3(transform.position.x - speed * Time.deltaTime,
+                transform.position.y, transform.position.z);
+
+            if (transform.position.x <= -14)
+            {
+                isMovingLeft = false;
+            }
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime,
+                transform.position.y, transform.position.z);
+
+            if (transform.position.x >= -1)
+            {
+                isMovingLeft = true;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.GetComponent<Animal>() && !col.gameObject.GetComponent<Animal>().isDead)
+        {
+            Destroy(col.gameObject);
+
+            audioSource.PlayOneShot(deathSound);
+        }
+    }
+}
